f075ef795417358662a964c9710500e72d7c0094
chan <chan9505@naver.com>
2024-04-20 15:28:03 +0900

    # 접근제한자, 캡슐화, 정보은닉

--- src/day07/encap/practice/Main.java
+++ src/day07/encap/practice/Main.java
@@ -37,5 +37,17 @@ public class Main {
         System.out.println("Car Brand: " + myCar.getBrand());
         System.out.println("Car Model: " + myCar.getModel());
         System.out.println("Car Year: " + myCar.getYear());
+
+
+        // Product 클래스 테스트
+        Product product = new Product();
+        product.setName("Laptop");
+        product.setPrice(1200000);
+        product.setStock(30);
+        System.out.println("상품명: " + product.getName());
+        System.out.println("상품 가격: " + product.getPrice());
+        System.out.println("재고 수량: " + product.getStock());
+        product.reduceStock(5);
+        System.out.println("판매 후 수량: " + product.getStock());
     }
 }
--- src/day07/encap/practice/Product.java
+++ src/day07/encap/practice/Product.java
@@ -1,4 +1,40 @@
 package day07.encap.practice;
 
 public class Product {
+
+    private String name;
+    private int price;
+    private int stock;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void reduceStock(int quantity) {
+        if (quantity < 0 || this.stock < quantity) {
+            System.out.println("요청한 수량이 재고보다 많거나 잘못된 수량입니다.");
+            return;
+        }
+            this.stock -= quantity;
+    }
 }
