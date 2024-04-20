2740ede54a291b5f513842c40f8f078caa696e10
chan <chan9505@naver.com>
2024-04-20 11:00:31 +0900

    # 상속 기초

---
+++ src/day07/inherit/Main.java
@@ -0,0 +1,15 @@
+package day07.inherit;
+
+import day07.modi.pac2.C;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator cal = new Calculator();
+        Computer com = new Computer();
+
+        System.out.println("계산기로 구한 원의넓이: " + cal.calcAreaCircle(5));
+        System.out.println("컴퓨터로 구한 원의넓이: " + com.calcAreaCircle(5));
+    }
+}
