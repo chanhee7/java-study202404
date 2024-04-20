43b50602c337df1491c6b4c2f8a35747447d2ca2
chan <chan9505@naver.com>
2024-04-20 15:07:16 +0900

    # 접근제한자, 캡슐화, 정보은닉

--- src/day07/encap/practice/Main.java
+++ src/day07/encap/practice/Main.java
@@ -11,5 +11,11 @@ public class Main {
         System.out.println("입금 후 잔액: " + myAccount.getBalance()); // 60000 출력
         myAccount.withdraw(20000);
         System.out.println("출금 후 잔액: " + myAccount.getBalance()); // 40000 출력
+
+
+        // PasswordManager 클래스 테스트
+        PasswordManager pm = new PasswordManager("oldPassword123");
+        boolean isChanged = pm.changePassword("oldPassword123", "newPassword123");
+        System.out.println("Password changed: " + isChanged); // true
     }
 }
