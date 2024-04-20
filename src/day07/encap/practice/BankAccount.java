866b57b8793f92f0c1313eaccc59c97390e1fa4b
chan <chan9505@naver.com>
2024-04-20 15:00:31 +0900

    # 접근제한자, 캡슐화, 정보은닉

--- src/day07/encap/practice/BankAccount.java
+++ src/day07/encap/practice/BankAccount.java
@@ -6,9 +6,30 @@ public class BankAccount {
     private String accountHolder;
     private long balance;
 
-
+    public BankAccount(String accountNumber, String accountHolder, long balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
 
     public long getBalance() {
         return balance;
     }
+
+    public void deposit(int balance) {
+        if (balance < 0) {
+            System.out.println("입금액은 음수일 수 없습니다.");
+            return;
+        }
+        this.balance += balance;
+    }
+
+    public void withdraw(int balance) {
+        if (balance < 0 || balance > this.balance) {
+            System.out.println("잔액이 부족하거나 잘못된 출금액입니다.");
+            return;
+        }
+        this.balance -= balance;
+    }
+
 }
--- src/day07/encap/practice/Main.java
+++ src/day07/encap/practice/Main.java
@@ -1,11 +1,15 @@
 package day07.encap.practice;
 
 public class Main {
-    // BankAccount 클래스 테스트
-//    BankAccount myAccount = new BankAccount("123-456-789", "김철수", 10000);
-//        System.out.println("현재 잔액: " + myAccount.getBalance());
-//        myAccount.deposit(50000);
-//        System.out.println("입금 후 잔액: " + myAccount.getBalance()); // 60000 출력
-//        myAccount.withdraw(20000);
-//        System.out.println("출금 후 잔액: " + myAccount.getBalance()); // 40000 출력
+
+    public static void main(String[] args) {
+
+        // BankAccount 클래스 테스트
+        BankAccount myAccount = new BankAccount("123-456-789", "김철수", 10000);
+        System.out.println("현재 잔액: " + myAccount.getBalance());
+        myAccount.deposit(50000);
+        System.out.println("입금 후 잔액: " + myAccount.getBalance()); // 60000 출력
+        myAccount.withdraw(20000);
+        System.out.println("출금 후 잔액: " + myAccount.getBalance()); // 40000 출력
+    }
 }
