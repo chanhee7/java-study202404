e6e85fad9602857a1f5cc8a50487ba1827931fa6
chan <chan9505@naver.com>
2024-04-20 10:59:55 +0900

    # 접근제한자, 캡슐화, 정보은닉

---
+++ src/day07/encap/practice/BankAccount.java
@@ -0,0 +1,14 @@
+package day07.encap.practice;
+
+public class BankAccount {
+
+    private String accountNumber;
+    private String accountHolder;
+    private long balance;
+
+
+
+    public long getBalance() {
+        return balance;
+    }
+}
