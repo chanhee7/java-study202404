98712f56948bb15ff5cb9ed2834d8093eb9f220e
chan <chan9505@naver.com>
2024-04-20 11:05:13 +0900

    # super, static

---
+++ src/day07/player/Player.java
@@ -0,0 +1,25 @@
+package day07.player;
+
+// 상위 클래스 (super class)
+// : 공통 속성(field)과 기능(method)을 가진 부모 클래스
+// Player 뒤에 extends Object 숨어있음
+public class Player {
+
+    String nickname;
+    int level;
+    int hp;
+
+    Player(String nickname) {
+        this.nickname = nickname;
+        this.level = 1;
+        this.hp = 50;
+    }
+
+    // 현재 캐릭터 정보를 출력
+    public void showStatus() {
+        System.out.println("\n======= Character's Info ========");
+        System.out.println("# name: " + this.nickname);
+        System.out.println("# level: " + this.level);
+        System.out.println("# hp: " + this.hp);
+    }
+}
